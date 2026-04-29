5a4cb4b1d66da99a982e56159c4f1da8cc4dae0a
Ben Compton <3343482+bencompton@users.noreply.github.com>
2026-04-29 11:19:26 -0600

    Clean up reducer naming

--- examples/todo-app/src/reducers/todoReducer.ts
+++ examples/todo-app/src/reducers/todoReducer.ts
@@ -6,21 +6,21 @@ export const initialState: ITodoAppState = {
 };
 
 export const todoReducer = createReducer<ITodoAppState>(initialState)
-    .bindAction(TodoActions.prototype.loadTodosSucceeded, (draft, action) => {
-        draft.todos = action.payload;
+    .bindAction(TodoActions.prototype.loadTodosSucceeded, (state, action) => {
+        state.todos = action.payload;
     })
-    .bindAction(TodoActions.prototype.addTodoSucceeded, (draft, action) => {
-        draft.todos.push(action.payload);
+    .bindAction(TodoActions.prototype.addTodoSucceeded, (state, action) => {
+        state.todos.push(action.payload);
     })
-    .bindAction(TodoActions.prototype.completeTodoSucceeded, (draft, action) => {
-        const todo = draft.todos.find(t => t.id === action.payload);
+    .bindAction(TodoActions.prototype.completeTodoSucceeded, (state, action) => {
+        const todo = state.todos.find(t => t.id === action.payload);
         if (todo) {
             todo.completed = true;
         }
     })
-    .bindAction(TodoActions.prototype.removeTodoSucceeded, (draft, action) => {
-        const index = draft.todos.findIndex(t => t.id === action.payload);
+    .bindAction(TodoActions.prototype.removeTodoSucceeded, (state, action) => {
+        const index = state.todos.findIndex(t => t.id === action.payload);
         if (index !== -1) {
-            draft.todos.splice(index, 1);
+            state.todos.splice(index, 1);
         }
     });
